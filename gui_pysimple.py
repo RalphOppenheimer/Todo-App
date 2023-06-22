f86d932610c7e9d27a792a55c15ad4d6faf37836
Ralph <rafal.kordaczek@gmail.com>
2023-06-22 12:34:11 +0200

    Gui_pysimple added entry box and add button

---
+++ gui_pysimple.py
@@ -0,0 +1,14 @@
+import modules.functions
+"""
+Can be used in tkinter, or designed in PAGE app. 
+"""
+import PySimpleGUI as sg
+
+label = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="Enter todo")
+add_button = sg.Button("Add")
+
+window = sg.Window("My to-do App", layout=[[label],
+                                           [input_box, add_button]])
+window.read()
+window.close()
