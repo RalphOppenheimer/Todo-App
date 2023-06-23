4dda885138d66d761fb5669041aecc11ebb8b501
Ralph <rafal.kordaczek@gmail.com>
2023-06-23 17:09:50 +0200

    create a static web interface

--- web.py
+++ web.py
@@ -9,6 +9,7 @@ st.write("This app is to increase your productivity")
 
 for todo in todos:
     st.checkbox(todo)
+st.radio(options="asd")
 
 st.text_input(label="", placeholder="Add new todo...")
 
