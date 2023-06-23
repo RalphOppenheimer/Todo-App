9f9e0a9e7359ce11b456763aca0c7c5f3c5f71ae
Ralph <rafal.kordaczek@gmail.com>
2023-06-23 09:39:37 +0200

    Change theme and clock implementation

--- gui_pysimple.py
+++ gui_pysimple.py
@@ -3,7 +3,11 @@ import modules.functions as fn
 Can be used in tkinter, or designed in PAGE app. 
 """
 import PySimpleGUI as sg
+import time
 
+sg.theme("Black")
+
+clock = sg.Text('', key='clock_key')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo_inbox_key")
 add_button = sg.Button("Add")
@@ -15,16 +19,15 @@ list_box = sg.Listbox(values=fn.get_todos(), key="todos_listbox_key",
                       enable_events=True, size=(45, 10))
 
 window = sg.Window("My to-do App",
-                   layout=[[label],
+                   layout=[[clock],
+                           [label],
                            [input_box, add_button],
                            [list_box, edit_button, complete_button]],
                    font=('Helvetica', 20))
 
 while True:
-    win_event, win_values = window.read()
-    print(1, win_event)
-    print(2, win_values)
-    print(3, win_values['todos_listbox_key'])
+    win_event, win_values = window.read(timeout=10)
+    window['clock_key'].update(value=time.strftime(" %A %Y%m%d-%H%M%S"))
     match win_event:
         case "Add":
             todos = fn.get_todos()
@@ -33,22 +36,28 @@ while True:
             fn.write_todos(todos)
             window['todos_listbox_key'].update(values=todos)
         case "Edit":
-            todo_to_edit = win_values['todos_listbox_key'][0]
-            new_todo = win_values['todo_inbox_key']
-            todos = fn.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo + '\n'
-            fn.write_todos(todos)
-            window['todos_listbox_key'].update(values=todos)
+            try:
+                todo_to_edit = win_values['todos_listbox_key'][0]
+                new_todo = win_values['todo_inbox_key']
+                todos = fn.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo + '\n'
+                fn.write_todos(todos)
+                window['todos_listbox_key'].update(values=todos)
+            except IndexError:
+                sg.popup("Please select an item first", font=('Helvetica', 20))
         case "Complete":
-            todo_to_complete = win_values['todos_listbox_key'][0]
-            todos = fn.get_todos()
-            todos.remove(todo_to_complete)
-            # index = todos.index(todo_to_complete)
-            # todos.pop(index)
-            fn.write_todos(todos)
-            window['todos_listbox_key'].update(values=todos)
-            window['todo_inbox_key'].update(value="")
+            try:
+                todo_to_complete = win_values['todos_listbox_key'][0]
+                todos = fn.get_todos()
+                todos.remove(todo_to_complete)
+                # index = todos.index(todo_to_complete)
+                # todos.pop(index)
+                fn.write_todos(todos)
+                window['todos_listbox_key'].update(values=todos)
+                window['todo_inbox_key'].update(value="")
+            except IndexError:
+                sg.popup("Please select an item first")
         case "Exit":
             break
         case "todos_listbox_key":
