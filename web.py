4a2b386a0f5a23ddcc0e4c7a6ae8598d754956db
Ralph <rafal.kordaczek@gmail.com>
2023-06-23 17:38:10 +0200

    Added new items implementation and print

--- web.py
+++ web.py
@@ -3,13 +3,20 @@ import functions as fn
 
 todos = fn.get_todos()
 
+def add_todo():
+    new_todo = st.session_state["new_todo"] + '\n'
+    print(new_todo)
+    todos.append(new_todo)
+    fn.write_todos(todos)
+
+
+
 st.title("My Todo App")
 st.subheader("This is my todo App")
 st.write("This app is to increase your productivity")
 
 for todo in todos:
     st.checkbox(todo)
-st.radio(options="asd")
 
-st.text_input(label="", placeholder="Add new todo...")
+st.text_input(label="Entry", placeholder="Add new todo...", on_change=add_todo, key="new_todo")
 
